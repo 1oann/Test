477ed47513f901ce3dcf46e1f38f3afdd8f36075
ioann <stck.ma1ls@gmail.com>
2025-01-19 19:31:52 +0300

    upd

---
+++ main/js/Условия.js
@@ -0,0 +1,40 @@
+if (1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error')
+} else if (num > 100) {
+    console.log('Много')
+} else {
+    console.log('Ok!')
+}
+
+// Тернарный оператор
+(num == 50) ? console.log('Ok!') : console.log('Error');
+
+// Бинарный аргумент (+)
+4 + 4
+
+// Унарный аргумент (+)
++'4';
+
+//
+switch (num) {
+    case 49: 
+        console.log('Неверно!');
+        break;
+    case 100:
+        console.log('Неверно!');
+        break;     
+    case 50:
+        console.log('Ок!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
